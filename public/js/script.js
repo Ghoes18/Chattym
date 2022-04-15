c7d0fb674fb5315f67f3d71a142ef8dbffa41c13
Gonçalo Miguel de Pereira Guimarães <goncalo.guimaraes@opencloud.pro>
2022-04-15 15:48:19 +0100

    Making the chat, and validations

--- public/js/script.js
+++ public/js/script.js
@@ -1,20 +1,25 @@
-const socket = io();
+const nickname = new URLSearchParams(window.location.search).get('nickname');
+const color = new URLSearchParams(window.location.search).get('color');
 
-let params = new URLSearchParams(location.search);
-let nickname = params.get("nickname");
-
-var messages = document.querySelector("#messages");
-var form = document.querySelector("#form");
-var input = document.querySelector("#input");
+const messages = document.querySelector("#messages");
+const form = document.querySelector("#form");
+const input = document.querySelector("#input");
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
   if (input.value) {
-    socket.emit("sendMessage", {
+    io().emit("sendMessage", {
       msg: input.value,
-      nickname: nickname,
+      nickname,
+      color,
     });
-    input.value = "";
-    alert("Message sent");
+    input.value = null;
   }
+});
+
+io().on('newMessage',(data) => {
+  const li = document.createElement("li");
+  li.innerHTML = `<span style="color:#${data.color}">${data.nickname}</span>: ${data.msg}`;
+  messages.appendChild(li);
+  window.scrollTo(0,document.body.scrollHeight);
 });
--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const path = require("path");
-const engines = require("consolidate");
 
 const app = express();
 const http = require("http").createServer(app);
@@ -8,8 +7,12 @@ const io = require("socket.io")(http);
 
 const port = process.env.PORT || 3000;
 
+const users = [];
+
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({
+  extended: true
+}));
 app.use('/public', express.static(path.join(__dirname, '../public')));
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
@@ -20,18 +23,34 @@ app.get("/", (req, res) => {
 
 app.post("/get_data", (req, res) => {
   const nickname = req.body.nickname;
-  res.redirect("/chat?nickname=" + nickname);
+  if(users.includes(nickname)){
+    res.send({ error: "Nickname already taken" });
+    return;
+  }
+  const randomColor = Math.floor(Math.random() * 16_777_215).toString(16);
+  users.push(nickname);
+  res.redirect(`/chat?nickname=${nickname}&color=${randomColor}`);
 });
 
 app.get("/chat", (req, res) => {
-  console.log(req.query.nickname);
-  res.render("chat", { nickname: req.query.nickname });
+  res.render("chat", {
+    nickname: req.query.nickname,
+    color: req.query.color
+  });
 });
 
 io.on("connection", (socket) => {
   socket.on("sendMessage", (data) => {
-    io.emit("newMessage", data);
+    if(users.includes(data.nickname)) {
+      io.emit("newMessage", data);
+    } else {
+      console.log("User not found");
+    }
   });
+});
+
+io.on("disconnect", (socket) => {
+  users.splice(id, 1);
 });
 
 http.listen(port, () => {
